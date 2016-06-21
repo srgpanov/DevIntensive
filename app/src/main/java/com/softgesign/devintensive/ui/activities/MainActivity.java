a60aedc1d6474a0322032b3477b06656cac6e402
srgpanov <srgpanov@rambler.ru>
2016-06-21 16:23:14 +0400

    Initial commit

---
+++ app/src/main/java/com/softgesign/devintensive/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.softgesign.devintensive.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softgesign.devintensive.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
